ad36156daa3c7d2641c954bd6ca2548191f609ec
luan magalhães <luan3642@hotmail.com>
2020-05-18 13:31:31 -0300

    exercicioNumerosOperadores

---
+++ numerosOperadoresExercicio.js
@@ -0,0 +1,36 @@
+//qual o resultado da seguinte expressão
+
+var total = 10 + 5 * 2 / 2 + 20;
+
+console.log(total);
+
+
+//crie duas expressões que retornem Nan
+
+var expressao1 = 'maluco' * 2;
+console.log(expressao1)
+var expressao2 = 'lala ' -2;
+console.log(expressao2);
+
+
+//Somar String '200' com o número 50 e retornar 250
+
+var string = '200';
+var number = 50;
+
+var soma = +string + 50;
+console.log(soma)
+
+//Incremente o valor 5 e retorne seu valor incrementado
+
+var x = 5;
+console.log(--x);
+
+// como dividir o peso por 2
+
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade ;
+
+
+console.log(peso)
