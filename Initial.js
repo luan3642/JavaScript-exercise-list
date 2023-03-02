09a0d43172cfad1a25f9f0464bd13ff368eabab7
LUAN MAGALHAES <luan3642@hotmail.com>
2023-03-01 21:34:50 -0300

    ...

--- Initial.js
+++ Initial.js
@@ -1,3 +1,15 @@
 function esconder(){
      document.getElementById("texto").style.display = "none"
 }
+
+function mudarText(){
+    document.getElementById("ei").innerHTML = "muda texto"
+}
+
+function definirHTML(){
+    document.getElementById("acess").innerHTML = 5+9
+}
+
+function mudarTexto(){
+    document.write(6+9)
+}
